dd724766b0d1414725f7b6206f7d0879bf599dd2
Michał Budzyński <michal@virtualdesign.pl>
2017-12-23 01:41:00 +0100

    :rugby_football: simplify

--- random_color.py
+++ random_color.py
@@ -2,7 +2,6 @@ import random
 
 def random_color():
 	r = lambda: random.randint(0,255)
-	hex = ''.join('%02x'%i for i in (r(), r(), r()))
-	return hex;
+	return ''.join('%02x'%i for i in (r(), r(), r()))
 
 print(random_color())
