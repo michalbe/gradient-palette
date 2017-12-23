d1be7ae7129f0480818d90325a87cabdbcc2083f
Michał Budzyński <michal@virtualdesign.pl>
2017-12-23 01:52:00 +0100

    :smile: gradient foundation

--- random_color.py
+++ random_color.py
@@ -7,4 +7,13 @@ def random_color():
 	r = lambda: random.randint(0,255)
 	return (r(), r(), r())
 
-print(to_hex(random_color()))
+
+def gradient(start, end, steps):
+	diff_r = (end[0] - start[0]) / steps
+	diff_g = (end[1] - start[1]) / steps
+	diff_b = (end[2] - start[2]) / steps
+	for x in range(0, steps):
+		print(x)
+
+
+gradient(random_color(), random_color(), 5)
