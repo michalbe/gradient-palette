b5a67e17bae46cefa9229c6e947c22c57141d12d
Michał Budzyński <michal@virtualdesign.pl>
2017-12-23 01:42:00 +0100

    :purse: hash at the beginning

--- random_color.py
+++ random_color.py
@@ -2,6 +2,6 @@ import random
 
 def random_color():
 	r = lambda: random.randint(0,255)
-	return ''.join('%02x'%i for i in (r(), r(), r()))
+	return '#' + ''.join('%02x'%i for i in (r(), r(), r()))
 
 print(random_color())
