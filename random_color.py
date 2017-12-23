4573414d0f5ab51c63bde90ed5f45ee60768f89e
Michał Budzyński <michal@virtualdesign.pl>
2017-12-23 02:04:00 +0100

    :evergreen_tree: working gradient code

--- random_color.py
+++ random_color.py
@@ -1,6 +1,7 @@
 import random
 
 def to_hex(rgb):
+	print(rgb)
 	return '#' + ''.join('%02x'%i for i in rgb).upper()
 
 def random_color():
@@ -9,11 +10,26 @@ def random_color():
 
 
 def gradient(start, end, steps):
-	diff_r = (end[0] - start[0]) / steps
-	diff_g = (end[1] - start[1]) / steps
-	diff_b = (end[2] - start[2]) / steps
-	for x in range(0, steps):
-		print(x)
+	diff_r = int((end[0] - start[0]) / steps)
+	print(diff_r)
+	r_delta = 1 if diff_r > 0 else -1
+	diff_g = int((end[1] - start[1]) / steps)
+	g_delta = 1 if diff_g > 0 else -1
+	diff_b = int((end[2] - start[2]) / steps)
+	b_delta = 1 if diff_b > 0 else -1
 
+	colors = list();
+	for i in range(1, steps - 1):
+		color = list()
+		color.append(r_delta * i * diff_r + start[0])
+		color.append(g_delta * i * diff_g + start[1])
+		color.append(b_delta * i * diff_b + start[2])
+		colors.append(to_hex(color))
+	return colors
 
-gradient(random_color(), random_color(), 5)
+first = random_color()
+second = random_color()
+
+print(first)
+print(second)
+print(gradient(first, second, 5))
