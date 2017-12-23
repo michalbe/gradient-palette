64e83027d4bb9d28261cd6f8e98fdcc42e57d489
Michał Budzyński <michal@virtualdesign.pl>
2017-12-23 02:17:00 +0100

    :bank: delta thingy fixed

--- random_color.py
+++ random_color.py
@@ -1,7 +1,6 @@
 import random
 
 def to_hex(rgb):
-	print(rgb)
 	return '#' + ''.join('%02x'%i for i in rgb).upper()
 
 def random_color():
@@ -10,21 +9,26 @@ def random_color():
 
 
 def gradient(start, end, steps):
-	diff_r = int((end[0] - start[0]) / steps)
-	print(diff_r)
-	r_delta = 1 if diff_r > 0 else -1
-	diff_g = int((end[1] - start[1]) / steps)
-	g_delta = 1 if diff_g > 0 else -1
-	diff_b = int((end[2] - start[2]) / steps)
-	b_delta = 1 if diff_b > 0 else -1
+	diff_r = round((end[0] - start[0]) / steps)
+	# r_delta = 1 if diff_r > 0 else -1
+	diff_g = round((end[1] - start[1]) / steps)
+	# g_delta = 1 if diff_g > 0 else -1
+	diff_b = round((end[2] - start[2]) / steps)
+	# b_delta = 1 if diff_b > 0 else -1
 
-	colors = list();
+	# print('delty r{0} g{1} b{2}'.format(r_delta, g_delta, b_delta))
+	print('r{0} g{1} b{2}'.format(diff_r, diff_g, diff_b))
+
+	colors = list()
+	colors.append(to_hex(start))
 	for i in range(1, steps - 1):
 		color = list()
-		color.append(r_delta * i * diff_r + start[0])
-		color.append(g_delta * i * diff_g + start[1])
-		color.append(b_delta * i * diff_b + start[2])
+		color.append(start[0] + i * diff_r)
+		color.append(start[1] + i * diff_g)
+		color.append(start[2] + i * diff_b)
+		print(color)
 		colors.append(to_hex(color))
+	colors.append(to_hex(end))
 	return colors
 
 first = random_color()
