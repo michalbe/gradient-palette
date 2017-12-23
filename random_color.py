a389d1d3c841cd8a1e5188e5f0d3cbe13bb38e08
Michał Budzyński <michal@virtualdesign.pl>
2017-12-23 01:40:00 +0100

    :train: make it functional

--- random_color.py
+++ random_color.py
@@ -1,4 +1,8 @@
 import random
 
-r = lambda: random.randint(0,255)
-print('#%02X%02X%02X' % (r(),r(),r()))
+def random_color():
+	r = lambda: random.randint(0,255)
+	hex = ''.join('%02x'%i for i in (r(), r(), r()))
+	return hex;
+
+print(random_color())
