aed5f6fd67973e498ac6c64fad45fc60b74e7532
Arnold Wildt <arnoldwildt@gmail.com>
2018-03-06 23:52:50 +0100

    Better images on larger scales (#2)

--- palette.py
+++ palette.py
@@ -13,36 +13,38 @@ def random_color():
 	return [r(), r(), r()]
 
 def gradient(start, end, steps):
-	diff_r = int((end[0] - start[0]) / steps)
-	diff_g = int((end[1] - start[1]) / steps)
-	diff_b = int((end[2] - start[2]) / steps)
+	diff_r = ((end[0] - start[0]) / steps)
+	diff_g = ((end[1] - start[1]) / steps)
+	diff_b = ((end[2] - start[2]) / steps)
 
 	colors = list()
 	colors.append(tuple(start))
 	for i in range(1, steps - 1):
 		color = list()
-		color.append(start[0] + i * diff_r)
-		color.append(start[1] + i * diff_g)
-		color.append(start[2] + i * diff_b)
+		color.append(int(start[0] + i * diff_r))
+		color.append(int(start[1] + i * diff_g))
+		color.append(int(start[2] + i * diff_b))
 		colors.append(tuple(color))
 	colors.append(tuple(end))
 	return tuple(colors)
 
-c00 = random_color()
-c10 = random_color()
-c01 = random_color()
-c11 = random_color()
+upleft = random_color()
+downleft = random_color()
+upright = random_color()
+downright = random_color()
 
 output = tuple()
-first_row = gradient(c00, c10, size)
-last_row = gradient(c01, c11, size)
+first_row = gradient(upleft, downleft, size)
+last_row = gradient(upright, downright, size)
 
 for i in range(0, size):
 	top_color = first_row[i]
 	bottom_color = last_row[i]
 	output = output + gradient(top_color, bottom_color, size)
-
+	
+imgname = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
 im = Image.new('RGB', (size, size))
 im.putdata(tuple(output))
 im = im.resize((final_scale, final_scale))
-im.save('images/' + ''.join(random.choice('0123456789ABCDEF') for i in range(16)) + '.png')
+im.save('images/' + imgname + '.png')
+print('Saved as: ' + imgname + '.png')
